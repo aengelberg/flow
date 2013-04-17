3307833994f65e33508b33e173ac9dab5f6615c5
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-04-17 12:54:40 -0700

    added batch-do function that solves several different puzzles in a directory

--- src/flow/core.clj
+++ src/flow/core.clj
@@ -1,6 +1,22 @@
-(ns flow.core)
+(ns flow.core
+  (:use flow.reader
+        flow.graphics))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn batch-do
+  [folder x y]
+  (doseq
+    [f (file-seq (clojure.java.io/file folder))
+     :when (not (.isDirectory f))]
+    (let [n (.getAbsolutePath f)
+          _ (println n)
+          a (time (solve-flow-graphic (file->grid n x y)))]
+      (if (= a nil)
+        (println false)
+        (println true))
+      )))
+
+;(time (solve-flow-graphic (file->grid "/home/alex/temp/sample11x11.PNG" 11 11)))
+
+;(batch-do "/home/alex/temp/Flowpuzzles/11x11/" 11 11)
+
+;(time (solve-flow-graphic (file->grid "/home/alex/temp/Flowpuzzles/11x11/IMG_1403.PNG" 11 11)))
