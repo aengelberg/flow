378132587a05b17fee2ee26ae9bc0ea03f983ee4
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-05-20 13:01:54 -0700

    now fails more elegantly when a solution is not found

--- src/flow/flowcore/search.clj
+++ src/flow/flowcore/search.clj
@@ -4,19 +4,23 @@
            java.util.concurrent.TimeUnit))
 
 (defn astar-search-helper
-  [queue neighbor-fn finish?-fn update-fn prom]
+  [queue neighbor-fn finish?-fn update-fn prom bored my-id]
   (loop []
-    (let [peeked (.poll queue 1000 TimeUnit/MILLISECONDS)
-          _ (if (and peeked
-                     (not (realized? prom)))
-              ((or update-fn identity) peeked))]
-      (cond
-        (realized? prom) nil
-        (not peeked) (deliver prom nil)
-        (finish?-fn peeked) (deliver prom [peeked])
-        :else (do (doseq [neigh (neighbor-fn peeked)]
-                    (.offer queue neigh))
-                (recur))))))
+    (if-not (every? identity @bored)
+      (let [peeked (.poll queue 50 TimeUnit/MILLISECONDS)
+            _ (if (and peeked
+                       (not (realized? prom)))
+                ((or update-fn identity) peeked))]
+        (if (not peeked)
+          (do (dosync (ref-set bored (assoc @bored my-id true)))
+            (recur))
+          (do
+            (dosync (ref-set bored (assoc @bored my-id false)))
+            (cond
+              (finish?-fn peeked) (deliver prom [peeked])
+              :else (do (doseq [neigh (neighbor-fn peeked)]
+                          (.offer queue neigh))
+                      (recur)))))))))
 
 (defn astar-search-updating
   [& {start-vals :start-vals
@@ -25,9 +29,11 @@
       update-fn :update-fn
       threads :threads}]
   (let [queue (PriorityBlockingQueue.)
-        prom (promise)]
+        prom (promise)
+        threads (or threads 1)
+        bored (ref (vec (repeat threads false)))]
     (doseq [thing start-vals]
       (.offer queue thing))
     (dotimes [i threads]
-      (on-thread #(astar-search-helper queue neighbors finished? update-fn prom)))
+      (on-thread #(astar-search-helper queue neighbors finished? update-fn prom bored i)))
     @prom))
