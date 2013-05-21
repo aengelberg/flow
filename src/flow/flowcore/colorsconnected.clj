c3b30ecaf587c1db11b74d1555bb1076ec3eb42e
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-05-20 19:56:02 -0700

    moved the union-find into a separate file and stopped using the java class

---
+++ src/flow/flowcore/colorsconnected.clj
@@ -0,0 +1,60 @@
+(ns flow.flowcore.colorsconnected
+  (:use flow.flowcore.base))
+
+(defn connected-components
+  [board]
+  (let [all-empty-posns (for [i (range (count board))
+                              j (range (count (first board)))
+                              :when (= \* (get-in board [i j]))]
+                          [i j])]
+    (loop [current-comp 0
+           comps {}
+           not-marked (set all-empty-posns)
+           stack [(first all-empty-posns)]] 
+      ;(println current-comp comps not-marked stack)
+      (cond
+        (and (empty? stack) (empty? not-marked)) comps
+        (empty? stack) (recur (inc current-comp)
+                              comps
+                              not-marked
+                              [(first not-marked)])
+        :else (let [posn (peek stack)
+                    color (get-in board posn)]
+                (cond
+                  (or (not (= color \*))
+                      (not (not-marked posn))) (recur current-comp
+                                                      comps
+                                                      not-marked
+                                                      (pop stack))
+                  :else (recur current-comp
+                               (assoc comps posn current-comp)
+                               (disj not-marked posn)
+                               (into (pop stack)
+                                     (quick-neighbors board posn)))))))))
+
+(defn colors-connected?
+  [this]
+  ;(println this)
+  (let [{board :board posns :posns} this
+        components (connected-components board)
+        comp->colors (atom {})
+        posn->comps (atom {})]
+    (doseq [[color [posn1 posn2]] posns
+            :when (not (adjacent? posn1 posn2))
+            posn [posn1 posn2]]
+      (let [comps (set (for [neigh (quick-neighbors board posn)
+                             :when (components neigh)]
+                         (components neigh)))]
+        (doseq [comp comps]
+          (swap! comp->colors assoc comp (conj (or (@comp->colors comp) ())
+                                             color)))
+        (swap! posn->comps assoc posn comps)))
+    ;(println comp->colors posn->comps)
+    (and
+      (every? identity (for [[color [p1 p2]] posns
+                             :when (not (adjacent? p1 p2))]
+                         (not (empty? (clojure.set/intersection
+                                        (@posn->comps p1) (@posn->comps p2))))))
+      (every? identity (for [[comp colors] @comp->colors]
+                         (let [freqs (frequencies colors)]
+                           (= (apply max (vals freqs)) 2)))))))
