2714df02f33c265c9742c102e438a84d509018e1
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-05-28 16:52:38 -0700

    moved batch-do to timing.clj, which has a new batch-solving suite

--- src/flow/core.clj
+++ src/flow/core.clj
@@ -3,24 +3,5 @@
         flow.graphics)
   (:require flow.seesaw))
 
-(defn batch-do
-  [folder x y]
-  (doseq
-    [f (file-seq (clojure.java.io/file folder))
-     :when (not (.isDirectory f))]
-    (let [n (.getAbsolutePath f)
-          _ (println n)
-          a (time (solve-flow-graphic (file->grid n x y)))]
-      (if (= a nil)
-        (println false)
-        (println true))
-      )))
-
-;(time (solve-flow-graphic (file->grid "/home/alex/temp/sample14x14.JPG" 14 14)))
-
-;(batch-do "/home/alex/temp/Flowpuzzles/11x11/" 11 11)
-
-;(time (solve-flow-graphic (file->grid "/home/alex/temp/Flowpuzzles/11x11/IMG_1391.PNG" 11 11)))
-
 (binding [flow.seesaw/*compile-gui* false]
   (flow.seesaw/-main))
