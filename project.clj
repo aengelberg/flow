66e87ca18da20a3b4fb861ef24902b6b88d5328a
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-06-02 14:06:55 -0700

    modified the version number and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject flow "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject flow "0.1.0"
+  :description "An unofficial solver for the iOS puzzle game \"Flow.\""
+  :url "http://github.com/aengelberg/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
